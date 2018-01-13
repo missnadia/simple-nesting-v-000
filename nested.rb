b21cdc6109a5372d6456e6027a39931b13c4c7a7
Nadia Lee <miss.nadia.lee@gmail.com>
2018-01-13 21:54:51 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -58,8 +58,7 @@ def dennis_ritchies_language
         }
      }
 
-programmer_hash[:dennis_ritchie][:languages]
-programmer_hash.to_s
+programmer_hash[:dennis_ritchie][:languages].to_s
 end
 
 def adding_matz
