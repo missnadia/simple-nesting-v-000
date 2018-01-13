db4202737217600f5327bfdfc6f52ce2d82780d3
Nadia Lee <miss.nadia.lee@gmail.com>
2018-01-13 21:45:13 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -21,6 +21,8 @@ end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+	programmer_hash[:alan_key][:known_for]
+	puts programmer_hash
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -36,7 +38,6 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-programmer_hash[:alan_key][:known_for]
 end
 
 def dennis_ritchies_language
